6f72bf956feeea4f4469282a49f71a0691c5e29d
Elisa <elisacanyelles@gmail.com>
2021-10-20 13:28:27 +0200

    files imported, added tasks inside the the global State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => { 
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "Feb 5th at 2:30 pm",
+        reminder: true,
+        
+    },
+    {
+        id: 2,
+        text: "Meeting at school",
+        day: "Feb 6th at 1:30 pm",
+        reminder: true,
+        
+    },
+    {
+        id: 3,
+        text: "Food shooping",
+        day: "Feb 5th at 2:30 pm",
+        reminder: false,
+        
+    }
+])
+ return (
+    <div className="container">
+    <Header />
+    <Tasks tasks ={tasks} />
     </div>
-  );
+  )
 }
 
 export default App;
