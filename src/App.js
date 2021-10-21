a8b96c8473276c24fe3e76e37422773d4ff4ccc1
Elisa <elisacanyelles@gmail.com>
2021-10-21 19:18:01 +0200

    using useState and filter method to  to delete task onClick

--- src/App.js
+++ src/App.js
@@ -27,10 +27,19 @@ const App = () => {
         
     }
 ])
+
+
+//delete task
+
+const deleteTask = (id) => {
+ setTasks(tasks.filter((task) => task.id !== id))
+}
+
+
  return (
     <div className="container">
     <Header />
-    <Tasks tasks ={tasks} />
+    <Tasks tasks ={tasks} onDelete={deleteTask}/>
     </div>
   )
 }
