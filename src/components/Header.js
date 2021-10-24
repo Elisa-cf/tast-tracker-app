95062fe2a83aeaac47e5e155f0839047725a239a
Elisa <elisacanyelles@gmail.com>
2021-10-24 11:09:42 +0200

    add dynamic color button red when shows form and blue when not

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const Header = ({title, onAdd, showAdd }) => {
     return (
       <header className="header">
           <h1>{title}</h1>
-          <Button color="green" 
+          <Button color={showAdd ? "red" : "blue"} 
           text = {showAdd ? "Close" : "Add"} 
           onClick={onAdd}/>
       </header>
